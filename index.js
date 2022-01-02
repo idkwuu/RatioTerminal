d85a59bb3fb78bc797b17de159fb20eb34e97359
idkwuu <me@idkwuu.dev>
2022-01-02 11:41:50 -0700

    Update license year

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2021 idkwuu
+Copyright (c) 2022 idkwuu
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
