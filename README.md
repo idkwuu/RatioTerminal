bd4aa89580f40f36fa77f7bf0548c8c6ee2a0a8a
idkwuu <me@idkwuu.dev>
2022-01-10 15:16:03 -0700

    🦕 Rewritten in Deno (and TypeScript)

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Ratio Terminal
 
-[Add this ratio](https://discord.com/api/oauth2/authorize?client_id=893355749441417287&permissions=67584&scope=bot)
+Remember Twitter ratios? Well, here these are for discord.
+
+Requires Deno (tested with v1.17.2).
+
+ `deno run --allow-net --allow-read --allow-env=RATIO_TERMINAL_TOKEN main.ts` 
+
+[Add this ratio](https://discord.com/api/oauth2/authorize?client_id=893355749441417287&permissions=67584&scope=bot) (keep in mind this bot is not verified, so if the bot reaches 75 servers, I'll shut it down and make a new one)
 
 ![Ratio](https://user-images.githubusercontent.com/42050584/140654544-6bc063e6-38ce-44d4-a151-be25f62eca1b.png)
---
+++ src/handleRatio.ts
@@ -0,0 +1,58 @@
+import { DiscordenoMessage } from "https://deno.land/x/discordeno@13.0.0-rc15/src/transformers/message.ts";
+import {addReaction, Bot, sendMessage} from "https://deno.land/x/discordeno@13.0.0-rc15/mod.ts";
+
+// <User ID, Date>
+const timeouts = new Map<bigint, Date | undefined>([])
+
+// <User ID, override>
+let overrides: Record<string, string> = {}
+try {
+    overrides = JSON.parse(await Deno.readTextFile("overrides.json"))
+// deno-lint-ignore no-unused-vars
+} catch (e) { 
+    // When the warning is sus!
+}
+
+export default function handleThisRatio(bot: Bot, message: DiscordenoMessage) {
+    const timeoutData: Date | undefined = timeouts.get(message.authorId) || undefined
+    
+    if (timeoutData != undefined) {
+        const timeDiff = new Date().getTime() - timeoutData.getTime()
+        // One ratio per minute
+        if (timeDiff >= 60000) {
+            timeouts.set(message.authorId, new Date())
+        } else {
+            addReaction(bot, message.channelId, message.id, "💀")
+                .catch((e) => console.log("Error reacting (bad ratio): " + e))
+            return
+        }
+    }
+
+    timeouts.set(message.authorId, new Date())
+
+    // Check for a ratio override
+    const ratioOverride: string | undefined = overrides[message.authorId.toString()]
+    let ratioResult: boolean
+    if (ratioOverride != undefined) {
+        ratioResult = ratioOverride === "accept"
+    } else {
+        ratioResult = Math.floor(Math.random() * 2) === 0
+    }
+
+    addReaction(bot, message.channelId, message.id, ratioResult ? "👍" : "👎")
+        .catch((e) => console.log("Error reacting (ratio reaction): " + e))
+    
+    sendMessage(bot, message.channelId, 
+        {
+            content: ratioResult
+                ? "https://docs.idkwuu.dev/ratioaccepted.png"
+                : "https://docs.idkwuu.dev/ratiodeclined.png",
+            messageReference: {
+                messageId: message.id,
+                failIfNotExists: true
+            }
+        }
+    )
+        .catch((e) => console.log("Error reacting (ratio image): " + e))
+
+}
